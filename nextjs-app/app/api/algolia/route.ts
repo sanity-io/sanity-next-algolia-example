df0c58ddca82ba509d250cf962d0438544d12ef2
irina <irina@sanity.io>
2024-11-14 15:37:48 -0500

    troubleshoot

--- nextjs-app/app/api/algolia/route.ts
+++ nextjs-app/app/api/algolia/route.ts
@@ -23,16 +23,14 @@ interface Block {
 }
 
 function toPlainText(blocks: Block[] = []) {
-  return (
-    blocks
-      .map((block) => {
-        if (block._type !== "block" || !block.children) {
-          return "";
-        }
-        return block.children.map((child) => child.text).join("");
-      })
-      .join("\n\n")
-  );
+  return blocks
+    .map((block) => {
+      if (block._type !== "block" || !block.children) {
+        return "";
+      }
+      return block.children.map((child) => child.text).join("");
+    })
+    .join("\n\n");
 }
 
 export async function POST(request: Request) {
@@ -45,34 +43,7 @@ export async function POST(request: Request) {
       console.warn("No JSON payload provided");
     }
 
-    // If no payload is present, perform a full reindex
-    // if (!payload) {
-    //   const sanityData = await sanityClient.fetch(`*[_type == "post"]{
-    //     _id,
-    //     title,
-    //     "path": slug.current,
-    //     content
-    //   }`);
-
-    //   const records = sanityData.map((doc: any) => ({
-    //     objectID: doc._id,
-    //     title: doc.title,
-    //     path: doc.path,
-    //     body: doc.content ? toPlainText(doc.content).slice(0, 9500) : '',
-    //   }));
-
-    //   await algoliaClient.saveObjects({
-    //     indexName,
-    //     objects: records,
-    //   });
-
-    //   console.log("Full reindex completed.");
-    //   return NextResponse.json({ message: "Successfully reindexed all objects!" });
-    // }
-
     const { _id, operation } = payload;
-    
-    console.log("Operation:", operation);
 
     if (operation === "delete") {
       await algoliaClient.deleteObject({
@@ -80,44 +51,40 @@ export async function POST(request: Request) {
         objectID: _id,
       });
       console.log(`Deleted object with ID: ${_id}`);
+      // If the operation is not delete, but create or update
     } else {
-      console.log(operation);
-      console.log('id', _id);
-      
+      const doc = await sanityClient.fetch(
+        `*[_id == $id][0]{
+          _id,
+          title,
+          "path": slug.current,
+          content
+        }`,
+        { id: _id }
+      );
+
+      if (!doc) {
+        return NextResponse.json(
+          { error: `Document with ID ${_id} not found in Sanity` },
+          { status: 404 }
+        );
+      }
+
+      const record = {
+        objectID: doc._id,
+        title: doc.title,
+      };
+
+      await algoliaClient.saveObject({
+        indexName,
+        body: record,
+      });
+      console.log(`Indexed object with ID: ${_id}`);
+
+      return NextResponse.json({
+        message: "Successfully processed operation!",
+      });
     }
-    // } else {
-    //   const doc = await sanityClient.fetch(
-    //     `*[_id == $id][0]{
-    //       _id,
-    //       title,
-    //       "path": slug.current,
-    //       content
-    //     }`,
-    //     { id: _id }
-    //   );
-
-    //   if (!doc) {
-    //     return NextResponse.json(
-    //       { error: `Document with ID ${_id} not found in Sanity` },
-    //       { status: 404 }
-    //     );
-    //   }
-
-    //   const record = {
-    //     objectID: doc._id,
-    //     title: doc.title,
-    //     path: doc.path,
-    //     body: doc.content ? toPlainText(doc.content).slice(0, 9500) : '',
-    //   };
-
-    //   await algoliaClient.saveObject({
-    //     indexName,
-    //     body: record,
-    //   });
-    //   console.log(`Indexed object with ID: ${_id}`);
-    // }
-
-    return NextResponse.json({ message: "Successfully processed operation!" });
   } catch (error: any) {
     console.error("Error indexing objects:", error.message);
     return NextResponse.json(
