f77c566816f470a049bdbf4fc746e50e572b492f
irina <irina@sanity.io>
2024-12-13 15:44:38 -0500

    add search box and logic to display results

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -2,7 +2,9 @@ import { Suspense } from "react";
 import Link from "next/link";
 
 import { AllPosts } from "@/app/components/Posts";
-import GetStartedCode from "@/app/components/GetStartedCode";
+import { Search } from "@/app/components/Search";
+
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   return (
@@ -25,28 +27,13 @@ export default async function Page() {
             </div>
             <div className="mt-6 space-y-6 prose sm:prose-lg md:prose-xl lg:prose-2xl text-gray-700">
               <p>
-              By integrating Sanity's structured content with Algolia, you can provide your users with fast, relevant search results and gather insights into what they are looking for.
+                By integrating Sanity's structured content with Algolia, you can provide your users with fast, relevant search results and gather insights into what they are looking for.
               </p>
             </div>
-            {/* <div className="flex items-center flex-col gap-4">
-              <GetStartedCode code="npm create sanity@latest" />
-              <Link
-                href="https://www.sanity.io/docs"
-                className="inline-flex text-red-500 text-xs md:text-sm underline hover:text-gray-900"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Sanity Documentation
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 ml-1 inline"
-                  fill="currentColor"
-                >
-                  <path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V12L17.206 8.207L11.2071 14.2071L9.79289 12.7929L15.792 6.793L12 3H21Z"></path>
-                </svg>
-              </Link>
-            </div> */}
+              <h3 className="p-2 mt-8 text-4xl font-semibold">Search with Algolia</h3>
+            <div className="mt-2">
+              <Search />
+            </div>
           </div>
         </div>
       </div>
