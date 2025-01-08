504255243fbaae4b95f4259c30f88e1f922ed111
irina <irina@sanity.io>
2025-01-08 10:21:17 -0500

    update git repo link

--- nextjs-app/app/components/Footer.tsx
+++ nextjs-app/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col gap-3 items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
             <a
-              href="https://github.com/sanity-io/sanity-template-nextjs-clean"
+              href="https://github.com/sanity-io/sanity-next-algolia-example"
               className="rounded-full flex gap-2 items-center bg-black hover:bg-red-500 focus:bg-cyan-500 py-3 px-6 text-white transition-colors duration-200"
               target="_blank"
               rel="noopener noreferrer"
